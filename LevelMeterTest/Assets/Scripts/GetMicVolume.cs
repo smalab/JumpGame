00f3844dcae363692a2b2c42f95249880a617f16
oitsmalab <oitsmalab@gmail.com>
2015-09-15 18:57:38 +0900

    Jumpgame #5 マイクの入力データを取得するスクリプトと音量を取得するスクリプトを作成。 メーターのオブジェクトに適用できていない状態

---
+++ LevelMeterTest/Assets/Scripts/GetMicVolume.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 空のAudio Sourceを作っておく
+[RequireComponent (typeof (AudioSource))]
+
+public class GetMicVolume : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		// 空のAudio Sourceを取得
+		var audio = GetComponent<AudioSource>();
+
+		// Audio SourceのAudio Clipをマイク入力に設定
+		// 引数は、デバイス名(nullならデフォルト)、ループ、何秒取るか、サンプリング周波数
+		audio.clip = Microphone.Start(null, false, 10, 44100);
+
+		// マイクがReadyになるまで待機
+		while (Microphone.GetPosition(null) <= 0) {}
+
+		// 再生開始(録った先から再生、スピーカーから出力するとハウリングするので注意)
+		audio.Play();
+	}
+}
