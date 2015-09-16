4666258217aed17200f265779b618374f4f86d9d
oitsmalab <oitsmalab@gmail.com>
2015-09-16 14:07:34 +0900

    Jumpgame #5 音量取得に関してのテスト中。 現状はまだ取得出来ていない状態。

--- LevelMeterTest/Assets/Scripts/GetAudioClipData.cs
+++ LevelMeterTest/Assets/Scripts/GetAudioClipData.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GetAudioClipData : MonoBehaviour {
 	public AudioClip clip;
-	public int lenghthYouWant;
+	public int lenghthYouWant = 1024;
 
 	// Use this for initialization
 	void Start () {
--- LevelMeterTest/Assets/Scripts/GetMicVolume.cs
+++ LevelMeterTest/Assets/Scripts/GetMicVolume.cs
@@ -12,7 +12,7 @@ public class GetMicVolume : MonoBehaviour {
 
 		// Audio SourceのAudio Clipをマイク入力に設定
 		// 引数は、デバイス名(nullならデフォルト)、ループ、何秒取るか、サンプリング周波数
-		audio.clip = Microphone.Start(null, false, 10, 44100);
+		audio.clip = Microphone.Start(null, true, 10, 44100);
 
 		// マイクがReadyになるまで待機
 		while (Microphone.GetPosition(null) <= 0) {}
@@ -20,4 +20,22 @@ public class GetMicVolume : MonoBehaviour {
 		// 再生開始(録った先から再生、スピーカーから出力するとハウリングするので注意)
 		audio.Play();
 	}
+
+		// Update is called once per frame
+	void Update () {
+		float vol = GetAveragedVolume();
+		Debug.Log(vol);
+	}
+
+		float GetAveragedVolume() {
+		var audio = GetComponent<AudioSource>();
+		float[] data = new float[256];
+		float a = 0;
+		audio.GetOutputData(data, 0);
+		foreach(float s in data) {
+			a += Mathf.Abs(s);
+		}
+		return a/256.0f;
+	}
+	
 }
