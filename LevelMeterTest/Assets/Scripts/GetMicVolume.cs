7418e497b029eda0b101a7a9cd7f5d401f57cb7f
oitsmalab <oitsmalab@gmail.com>
2015-09-16 16:28:16 +0900

    Jumpgame #5 新しくスクリプトを書き直してみましたが、結果は変わらず。 ログで音量の確認を取るようにしているのですが、ずっと0の状態で変化しません。

--- LevelMeterTest/Assets/Scripts/GetMicVolume.cs
+++ LevelMeterTest/Assets/Scripts/GetMicVolume.cs
@@ -1,41 +1,74 @@
-﻿using UnityEngine;
+﻿// 参考URL:http://unity-michi.com/post-396/
+
+using UnityEngine;
 using System.Collections;
 
 // 空のAudio Sourceを作っておく
 [RequireComponent (typeof (AudioSource))]
+[DisallowMultipleComponent]	// 複数アタッチを禁止
 
 public class GetMicVolume : MonoBehaviour {
+	// 外部から現在の音量を読み取る
+	// 最大値はsensitivityによる
+	public float GetLoudness() {
+		return loudness;
+	}
+	
+	public float sensitibity = 100;	// 感度,音量の最大値
+	
+	float loudness;    // 音量
+	float lastLoudness;// 前フレームの音量
+	
+	[Range(0,0.95f)]   // 最大1に出来ると全く変動しなくなるのを防ぐ
+	public float lastLoudnessInfluence;// 前フレームの影響度合い
+
+	// Use this for initialization
+	// void Start () {
+	// }
+
+	// void InitRecord() {
+	// 	var audio = GetComponent<AudioSource>();
+	// 	audio.clip = Microphone.Start(null, true, 10, 44100);
+	// 	audio.loop = true;
+	// 	audio.mute = true;
+	// 	while(!(Microphone.GetPosition("") > 0)){}
+	// 	audio.Play();
+	// }
+
 	// Use this for initialization
 	void Start () {
-		// 空のAudio Sourceを取得
 		var audio = GetComponent<AudioSource>();
-
-		// Audio SourceのAudio Clipをマイク入力に設定
-		// 引数は、デバイス名(nullならデフォルト)、ループ、何秒取るか、サンプリング周波数
-		audio.clip = Microphone.Start(null, true, 10, 44100);
-
-		// マイクがReadyになるまで待機
-		while (Microphone.GetPosition(null) <= 0) {}
-
-		// 再生開始(録った先から再生、スピーカーから出力するとハウリングするので注意)
+		audio.clip = Microphone.Start("testmic (USB2.0 MIC)", true, 999, 44100);
+		audio.loop = true;
+		audio.mute = true;
+		while (!(Microphone.GetPosition("") > 0)) {}
 		audio.Play();
 	}
 
-		// Update is called once per frame
+	// Update is called once per frame
 	void Update () {
+		CalcLoudness();
 		float vol = GetAveragedVolume();
 		Debug.Log(vol);
 	}
 
-		float GetAveragedVolume() {
-		var audio = GetComponent<AudioSource>();
+	// 現フレームの音量を計算する
+	// マイク感度が過剰な時はlastLoudnessInfluenceを上げたりして調整すること
+	void CalcLoudness() {
+		lastLoudness = loudness;
+		loudness = GetAveragedVolume() * sensitibity
+			* (1 - lastLoudnessInfluence) + lastLoudness * lastLoudnessInfluence;
+	}
+
+	float GetAveragedVolume() {
 		float[] data = new float[256];
 		float a = 0;
+		var audio = GetComponent<AudioSource>();
 		audio.GetOutputData(data, 0);
-		foreach(float s in data) {
+		foreach (float s in data) {
 			a += Mathf.Abs(s);
 		}
-		return a/256.0f;
+		return a/256;
 	}
-	
+
 }
