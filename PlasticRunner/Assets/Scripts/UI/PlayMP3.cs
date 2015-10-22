7d5ab1a8b01a198a6d23568ccfbb371236c4046f
kamikuzu <kamikuzu@outlook.com>
2015-10-22 16:43:41 +0900

    プレイヤーが看板と当たってもプレイヤーが跳ね返らないようにしました。 プレイヤーが着地時にスピードが落ちていたのを修正しました。

--- PlasticRunner/Assets/Scripts/UI/PlayMP3.cs
+++ PlasticRunner/Assets/Scripts/UI/PlayMP3.cs
@@ -2,9 +2,6 @@
 using System.Collections;
 
 public class PlayMP3 : MonoBehaviour {
-
-	public GameObject player;
-	public GameObject Sign;
 	public float speed = 0.07f;
 	public AudioClip audioclip;
 	AudioSource audiosource;
@@ -13,21 +10,21 @@ public class PlayMP3 : MonoBehaviour {
 	void Start () {
 		audiosource = gameObject.GetComponent<AudioSource> ();
 		audiosource.clip = audioclip;
-		player = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (unity.transform.position - transform.position), speed);
-		// transform.position += transform.forward * speed;
 	}
 
-	void OnCollisionEnter(Collision col) {
-		if (col.gameObject.name == "Canvas Sign") {
+	void OnTriggerEnter(Collider col) {
+		// if (col.gameObject.name == "Canvas Sign") {
+			// audiosource.PlayOneShot (audioclip);
+			// GameObject.Destroy(Sign);
+		//}
+
+		if (col.gameObject.layer == LayerMask.NameToLayer("Sign")) {
 			audiosource.PlayOneShot (audioclip);
-			// Debug.Log ("entertag");
-			GameObject.Destroy(Sign);
-			// gameObject.GetComponent<Hitbucket> ().enabled = false;
+			GameObject.Destroy(col.gameObject);
 		}
 	}
 }
