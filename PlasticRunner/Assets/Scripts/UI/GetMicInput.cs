b32037eab48465d9bab1a37ac2797d08288546ca
kamikuzu <kamikuzu@outlook.com>
2015-10-27 15:08:57 +0900

    JumpGame #7 制限時間を5分に設定して音量データ取得のテスト。 途中でエラーが発生してしまい、5分間プレーできない状態。

--- PlasticRunner/Assets/Scripts/UI/GetMicInput.cs
+++ PlasticRunner/Assets/Scripts/UI/GetMicInput.cs
@@ -4,9 +4,11 @@ using System.Collections;
 // 空の Audio Source を作って置く
 [RequireComponent (typeof (AudioSource))]
 public class GetMicInput : MonoBehaviour {
-    static public float loudness;             //音量.
     float lastLoudness;         //前フレームの音量.
-    public float sensitivity = 100;     //感度.音量の最大値.
+    float m_aveVoice;       //平均的な声の音量
+    public float second = 300.0f;
+    static public float loudness;             //音量.
+    public float sensitivity = 100.0f;     //感度.音量の最大値.
     public float lastLoudnessInfluence; //前フレームの影響度合い.
 
     void Start() 
@@ -15,7 +17,7 @@ public class GetMicInput : MonoBehaviour {
         var audio = GetComponent<AudioSource>();
         // Audio Source の Audio Clip をマイク入力に設定
         // 引数は、デバイス名（null ならデフォルト）、ループ、何秒取るか、サンプリング周波数
-        audio.clip = Microphone.Start(null, false, 30, 44100);
+        audio.clip = Microphone.Start(null, false, 300, 44100);
         // マイクが Ready になるまで待機（一瞬）
         while (Microphone.GetPosition(null) <= 0) {}
         // 再生開始（録った先から再生、スピーカーから出力するとハウリングします）
@@ -24,7 +26,7 @@ public class GetMicInput : MonoBehaviour {
 
     void Update () {
         CalcLoudness();
-        //Debug.Log(loudness);
+        CalcAveVoiceVolume();
     }
  
     //現フレームの音量を計算します.
@@ -32,6 +34,17 @@ public class GetMicInput : MonoBehaviour {
     void CalcLoudness() {
         lastLoudness = loudness;
         loudness = GetAveragedVolume() * sensitivity * ( 1 - lastLoudnessInfluence ) + lastLoudness * lastLoudnessInfluence;
+    }
+
+    //ユーザーの声の平均的な音量を計算する
+    public void CalcAveVoiceVolume ()
+    {
+        m_aveVoice += lastLoudness;
+        second -= Time.deltaTime;
+        if (second <= 0.0f)
+        {
+            m_aveVoice /= second;
+        }
     }
  
     //現フレームで再生されている AudioClip から平均的な音量を取得します.
