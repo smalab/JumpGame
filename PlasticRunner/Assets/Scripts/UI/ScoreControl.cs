fc0fe11dbb19b1196af801702087eee9d77e5e70
kamikuzu <kamikuzu@outlook.com>
2015-11-05 14:02:20 +0900

    JampGame #8 スコア機能の実装。スクリプト作成の途中。

---
+++ PlasticRunner/Assets/Scripts/UI/ScoreControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreControl : MonoBehaviour {
+    int mScore = 0;
+    
+	// Use this for initialization
+	void Start () {
+	    GetComponent.text
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void AddScore()
+    {
+
+    }
+}
