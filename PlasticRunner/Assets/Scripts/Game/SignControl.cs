6e813bea3b6cc16823471b981d7feeed33202db2
kamikuzu <kamikuzu@outlook.com>
2015-11-05 15:31:40 +0900

    JumpGame #8 スコア実装のつづき。ScoreControlではなぜかクラス参照を行うことが出来なかったため、 スクリプト名をScoreManagerに変更したら参照できるようになりました。

--- PlasticRunner/Assets/Scripts/Game/SignControl.cs
+++ PlasticRunner/Assets/Scripts/Game/SignControl.cs
@@ -3,18 +3,21 @@ using System.Collections;
 
 public class SignControl : MonoBehaviour {
     private PlayMP3 mPlaymp3 = null;
+    private ScoreManager mScorecontrol = null;
 
     void Start()
     {
         mPlaymp3 = GameObject.Find("AudioManager").GetComponent<PlayMP3>();
+        mScorecontrol = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
     }
 
     void OnTriggerEnter (Collider collider)
     {
         if (collider.gameObject.layer == LayerMask.NameToLayer("Event"))
         {
-           mPlaymp3.SendMessage("Playmp3");
-           Destroy(gameObject, 1.0f);
+            mPlaymp3.SendMessage("Playmp3");
+            mScorecontrol.SendMessage("CallAddScore");
+            Destroy(gameObject, 1.0f);
         }
     }
 }
---
+++ PlasticRunner/Assets/Scripts/UI/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    int mScore = 0;
+    
+	// Use this for initialization
+	void Start () {
+        GetComponent<Text>().text = mScore.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+    public void CallAddScore()
+    {
+
+        StartCoroutine("AddScore");
+    }
+
+    IEnumerator AddScore()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            mScore += 2;
+            GetComponent<Text>().text = mScore.ToString();
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
