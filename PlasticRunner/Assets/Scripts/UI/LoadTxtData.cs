2922cb54010ad95b57bc3cc8a553a64aba3af960
kamikuzu <kamikuzu@outlook.com>
2015-11-12 17:13:01 +0900

    JumpGame #11 配列に格納したアルファベットをランダムに取得し、看板のテキストに出力させる。 実行するとNullReferenceが出て再生が止まるがデバッグの仕方が悪いだけのはず。

--- PlasticRunner/Assets/Scripts/UI/LoadTxtData.cs
+++ PlasticRunner/Assets/Scripts/UI/LoadTxtData.cs
@@ -1,27 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 
 public class LoadTxtData : MonoBehaviour {
     public TextAsset AlphabetTextAsset = null;
+    string AlphabetText = "";
+    string[] lines = null;
+    int idx = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+        LoadText();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        //this.GetComponent<Text>().text = "test";
-        LoadText();
-	}
+        ShowText();
+        Debug.Log(idx);
+        Debug.Log(lines[idx]);
+    }
 
     void LoadText ()
     {
         AlphabetTextAsset = Resources.Load("text/Alphabet") as TextAsset;
-        string AlphabetText = AlphabetTextAsset.text;
-        string[] lines = AlphabetText.Split('\n');
-        foreach(var line in lines) Debug.Log(line);
+        AlphabetText = AlphabetTextAsset.text;
+        lines = AlphabetText.Split('\n');
     }
 
+    void ShowText()
+    {
+        System.Random rnd = new System.Random();
+        idx = rnd.Next(lines.Length);
+        GetComponent<Text>().text = lines[idx];
+    }
 }
