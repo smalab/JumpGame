60880a1c324f9f78a0d95df51783a80272ee0bfa
kamikuzu <kamikuzu@outlook.com>
2015-12-03 16:00:10 +0900

    JumpGame #12 音量の取得タイミングをジャンプ中に修正中。 NullReferenceが出てしまって上手く動作していない状態。

--- PlasticRunner/Assets/Scripts/Game/PlayerControl.cs
+++ PlasticRunner/Assets/Scripts/Game/PlayerControl.cs
@@ -30,86 +30,91 @@ public class PlayerControl : MonoBehaviour {
 	private float click_timer = -1.0f;	// ボタンが押されてからの時間
 	private float CLICK_CRACE_TIME = 0.5f;	// ジャンプしたい意志を受け取る時間
 	private Rigidbody mRigidbody = null;
+	private csv_GetVolume mCsvWriter = null;
 	public PlayerButtom mButtom = null;
 
 	// Use this for initialization
 	void Start () {
-		this.next_step = STEP.RUN;	// ゲーム開始を走る状態に
-		mRigidbody = this.GetComponent<Rigidbody>();
-		mButtom = this.GetComponentInChildren<PlayerButtom>();
+		next_step = STEP.RUN;	// ゲーム開始を走る状態に
+		mRigidbody = GetComponent<Rigidbody>();
+		mButtom = GetComponentInChildren<PlayerButtom>();
+		//mButtom = Resources.Load("Prefabs/Player/Buttom") as PlayerButtom;
+		mCsvWriter = GetComponent<csv_GetVolume>();
+		mCsvWriter.Start();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		Vector3 velocity = mRigidbody.velocity;	// 速度を設定
-		this.current_speed = this.level_control.getPlayerSpeed();
-		this.check_landed();	// 着地状態かどうかをチェック
+		current_speed = level_control.getPlayerSpeed();
+		check_landed();	// 着地状態かどうかをチェック
 
-		switch (this.step) {
+		switch (step) {
 			case STEP.RUN:
 			case STEP.JUMP:
 				// 現在の位置がしきい値よりも下ならば
-				if(this.transform.position.y < NARAKU_HEIGHT) {
-					this.next_step = STEP.MISS;	// ミス状態にする
+				if(transform.position.y < NARAKU_HEIGHT) {
+					next_step = STEP.MISS;	// ミス状態にする
 				}
 				break;
 		}
 
-		this.step_timer += Time.deltaTime;	// 経過時間を進める
+		step_timer += Time.deltaTime;	// 経過時間を進める
 
 		if(Input.GetMouseButtonDown(0)) {	// ボタンが押されたら
-			this.click_timer = 0.0f;	// タイマーをリセット
+			click_timer = 0.0f;	// タイマーをリセット
 		} else {
-			if(this.click_timer >= 0.0f) {	// そうでなければ
-				this.click_timer += Time.deltaTime;	// 経過時間を加算
+			if(click_timer >= 0.0f) {	// そうでなければ
+				click_timer += Time.deltaTime;	// 経過時間を加算
 			}
 		}
 
 		// 次の状態が決まっていなければ、状態の変化を調べる
-		if(this.next_step == STEP.NONE) {
-			switch(this.step) {	// Playerの現在の状態で分岐
+		if(next_step == STEP.NONE) {
+			switch(step) {	// Playerの現在の状態で分岐
 				case STEP.RUN:	// 走行中の場合
 					// click_timerが0以上、CLICK_GRACE_TIME以下の場合
-					if(0.0f <= this.click_timer && this.click_timer <= CLICK_CRACE_TIME) {
-						if(this.mButtom.IsLanded == true /*&& GetMicInput.loudness >= 2*/) {	// 着地しているならば
-							this.click_timer = -1.0f;	// ボタンが押されていないことを表す-1.0fに
-							this.next_step = STEP.JUMP;	// ジャンプ状態に
+					if(0.0f <= click_timer && click_timer <= CLICK_CRACE_TIME) {
+						if(mButtom.IsLanded == true) {	// 着地しているならば
+							click_timer = -1.0f;	// ボタンが押されていないことを表す-1.0fに
+							next_step = STEP.JUMP;	// ジャンプ状態に
 							}
 						}
 					break;
 				case STEP.JUMP:	// ジャンプ中の場合
-					if(this.mButtom.IsLanded == false) {
+					mCsvWriter.LogSave();
+					if(mButtom.IsLanded == false) {
 						// ジャンプ中で着地していたら、次の状態を走行中に変更
-						this.next_step = STEP.RUN;
+						next_step = STEP.RUN;
 					}
 					break;
 			}
 		}
 
 		// 次の状態が状態情報なし以外の間
-		while(this.next_step != STEP.NONE) {
-			this.step = this.next_step;	// 現在の状態を次の状態に更新
-			this.next_step = STEP.NONE;	// 次の情愛を状態なしに変更
-			switch(this.step) {	// 更新された現在の状態が
+		while(next_step != STEP.NONE) {
+			step = next_step;	// 現在の状態を次の状態に更新
+			next_step = STEP.NONE;	// 次の情愛を状態なしに変更
+			switch(step) {	// 更新された現在の状態が
 				case STEP.JUMP:	// ジャンプの場合
 					// ジャンプの高さからジャンプの初速を計算
 					velocity.y = Mathf.Sqrt(2.0f * 9.8f * PlayerControl.JUMP_HEIGHT_MAX);
 					// ボタンが離されたフラグをクリアする
-					this.is_key_released = false;
+					is_key_released = false;
 					break;
 			}
-			this.step_timer = 0.0f;	// 状態が変化したので、経過時間をリセット
+			step_timer = 0.0f;	// 状態が変化したので、経過時間をリセット
 		}
 
 		// 状態ごとの、毎フレームの更新処理
-		switch(this.step) {
+		switch(step) {
 			case STEP.RUN:	// 走行中の場合
 				// 速度を上げる
 				velocity.x += PlayerControl.ACCELERATION * Time.deltaTime;
 				// 計算結果のスピードが設定すべきスピードを超えていたら
-				if(Mathf.Abs(velocity.x) > this.current_speed) {
+				if(Mathf.Abs(velocity.x) > current_speed) {
 					// 超えないように調整
-					velocity.x *= this.current_speed / Mathf.Abs(velocity.x);
+					velocity.x *= current_speed / Mathf.Abs(velocity.x);
 				}
 				break;
 			case STEP.JUMP:	// ジャンプ中の場合
@@ -119,7 +124,7 @@ public class PlayerControl : MonoBehaviour {
 						break;
 					}
 					// 減速済みなら　(複数回減速しないように)
-					if(this.is_key_released) {
+					if(is_key_released) {
 						break;
 					}
 					// 上下方向の速度が0以下なら（下降中なら）
@@ -129,7 +134,7 @@ public class PlayerControl : MonoBehaviour {
 					// ボタンが離されていて、上昇中なら減速開始
 					// ジャンプの上昇はここで終了
 					velocity.y *= JUMP_KEY_RELEASE_REDUCE;
-					this.is_key_released = true;
+					is_key_released = true;
 				} while(false);
 				break;
 			case STEP.MISS:
@@ -150,21 +155,21 @@ public class PlayerControl : MonoBehaviour {
 	}
 
 	private void check_landed() {
-		// this.is_landed = false;
+		// is_landed = false;
 
 		do {
 			// Vector3 s = this.transform.position;	// Playerの現在位置
 			// Vector3 e = s + Vector3.down * 1.0f;	// sから1.0fに移動した位置
 
 			// RaycastHit hit;
-			if(this.mButtom.IsLanded == false) {	// sからeの間に何もない場合
+			if(mButtom.IsLanded == false) {	// sからeの間に何もない場合
 				break;	// 何もせずdo-whileループを抜ける
 			}
 
 			// sからeの間に何かがあった場合の処理
-			if(this.step == STEP.JUMP) {	// 現在がジャンプ状態ならば
+			if(step == STEP.JUMP) {	// 現在がジャンプ状態ならば
 				// 経過時間が3.0f未満ならば
-				if(this.step_timer < Time.deltaTime * 3.0f) {
+				if(step_timer < Time.deltaTime * 3.0f) {
 					break;
 				}
 			}
--- PlasticRunner/Assets/Scripts/UI/csv_GetVolume.cs
+++ PlasticRunner/Assets/Scripts/UI/csv_GetVolume.cs
@@ -1,17 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.IO;
-using System;
 using System.Text;
 
 public class csv_GetVolume : MonoBehaviour {
 
-	private float mTime = 0.0f;
-    GetMicInput AverageVoice = null;
+	//private float mTime = 0.0f;
+    GetMicInput mAverageVoice = null;
 
 	// Use this for initialization
-	void Start () {
-        AverageVoice = GetComponent<GetMicInput>();
+	public void Start () {
+        mAverageVoice = GetComponent<GetMicInput>();
 		if (File.Exists("Assets/log.csv")) {
 			// Debug.Log("fileatta");
 			FileStream f = new System.IO.FileStream("Assets/log.csv", FileMode.Append, FileAccess.Write);
@@ -25,20 +23,21 @@ public class csv_GetVolume : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//0.2s gotoni data wo toru
-		mTime += Time.deltaTime;
-		if (mTime >= 0.1f) {
-			logSave ( GetMicInput.loudness.ToString(), AverageVoice.aveVoice.ToString() );
-			mTime = 0.0f;
-		}
+		// mTime += Time.deltaTime;
+		// if (mTime >= 0.1f) {
+		// 	logSave ( GetMicInput.loudness.ToString(), AverageVoice.aveVoice.ToString() );
+		// 	mTime = 0.0f;
+		// }
+
 
 	}
 
-	public void logSave(string PlayVolume, string VoiceVolume){
-		FileStream f = new FileStream("Assets/log.csv", FileMode.Append, FileAccess.Write);
+	public void LogSave(){
+		FileStream mLogSave = new FileStream("Assets/log.csv", FileMode.Append, FileAccess.Write);
 		Encoding utf8Enc = Encoding.GetEncoding("UTF-8");
-		StreamWriter writer = new StreamWriter(f, utf8Enc);
-		writer.Write(PlayVolume + ",");
-        writer.Write(VoiceVolume + ",");
+		StreamWriter writer = new StreamWriter(mLogSave, utf8Enc);
+		writer.Write(GetMicInput.loudness.ToString() + ",");
+        writer.Write(mAverageVoice.aveVoice.ToString() + ",");
         writer.Write(" " + ",");
 		writer.Close();
 	}
