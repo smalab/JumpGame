66b035d1bac67e5308fd1ba281991a82cb4551cd
oitsmalab <oitsmalab@gmail.com>
2015-12-17 16:14:15 +0900

    #12 音声の長さを取得しようとしている最中。 予め設定した長さ(今はゲームを遊んでもらうための300s)を取得してしまう。 できれば録音ボタンを使わずに実装したいのだが・・・

--- PlasticRunner/Assets/Scripts/UI/GetMicInput.cs
+++ PlasticRunner/Assets/Scripts/UI/GetMicInput.cs
@@ -6,30 +6,32 @@ using System.Collections;
 public class GetMicInput : MonoBehaviour {
     float lastLoudness;         //前フレームの音量.
     public static float m_aveVoice;       //平均的な声の音量
-    public float second = 300.0f;
-    static public float loudness;             //音量.
+    public float second = 300.0f;   //録音する音データの長さ(s)
+    static public float loudness;             //現フレームの音量.
     public float sensitivity = 100.0f;     //感度.音量の最大値.
     public float lastLoudnessInfluence; //前フレームの影響度合い.
+    private AudioSource mAudio = null;
 
     //public float AveVoice {get { return m_aveVoice; } }
 
     void Start() 
     {
         // 空の Audio Sourceを取得
-        var audio = GetComponent<AudioSource>();
+        mAudio = GetComponent<AudioSource>();
         // Audio Source の Audio Clip をマイク入力に設定
         // 引数は、デバイス名（null ならデフォルト）、ループ、何秒取るか、サンプリング周波数
-        audio.clip = Microphone.Start("Microphone", false, 300, 44100);
+        mAudio.clip = Microphone.Start("Microphone", false, 300, 44100);
         // マイクが Ready になるまで待機（一瞬）
         while (Microphone.GetPosition(null) <= 0) {}
         // 再生開始（録った先から再生、スピーカーから出力するとハウリングします）
-        audio.Play();
+        mAudio.Play();
     }
 
     void Update () {
         CalcLoudness();
         CalcAveVoiceVolume();
         Debug.Log(loudness);
+        Debug.Log(mAudio.clip.length);
     }
  
     //現フレームの音量を計算します.
