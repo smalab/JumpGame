1548194084c8177de04bed6595ef2e56d244d7fa
oitsmalab <oitsmalab@gmail.com>
2016-01-25 14:09:18 +0900

    ゲーム内の細部修正。 看板の新規作成と床ブロックの変更を行いました。

---
+++ PlasticRunner/Assets/TextMeshTester.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextMeshTester : MonoBehaviour {
+    TextMesh mTextMesh = null;
+	// Use this for initialization
+	void Start () {
+        mTextMesh = GetComponent<TextMesh>();
+        mTextMesh.text = "A";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
