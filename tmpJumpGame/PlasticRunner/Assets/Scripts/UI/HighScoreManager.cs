4cc2690044fd230be98b33096c5789762730d652
oitsmalab <oitsmalab@gmail.com>
2016-02-03 16:08:45 +0900

    ハイスコア機能の追加を試作中。

---
+++ tmpJumpGame/PlasticRunner/Assets/Scripts/UI/HighScoreManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScoreManager : MonoBehaviour
+{
+    private string mRankingPrefKey = "ranking";
+    private int mRankingNum = 5;
+    private int[] mRankedScore = null;
+
+    // Use this for initialization
+    void Start() {}
+
+    // Update is called once per frame
+    void Update() {}
+
+    //ランキングをPlayerPrefsから取得
+    //mRankedScoreに格納
+    void GetRanking()
+    {
+        var _ranking = PlayerPrefs.GetString(mRankingPrefKey);
+        if(_ranking.Length > 0)
+        {
+            string[] _score = _ranking.Split(","[0]);
+            mRankedScore = new int[mRankingNum];
+            for(int i = 0; i < _score.Length && i < mRankingNum; i++)
+            {
+                mRankedScore[i] = int.Parse(_score[i]);
+            }
+        }
+    }
+
+    //新しくスコアを保存する
+    void SaveRanking(int aNewScore)
+    {
+        if (mRankedScore.Length != 0)
+        {
+            int _tmp = 0;
+            for(int i= 0; i < mRankedScore.Length; i++)
+            {
+                if(mRankedScore[i] < aNewScore)
+                {
+                    _tmp = mRankedScore[i];
+                    mRankedScore[i] = aNewScore;
+                    aNewScore = _tmp;
+                }
+            }
+        }
+        else
+        {
+            mRankedScore[0] = aNewScore;
+        }
+    }
+
+    //ランキングの削除
+    
+}
